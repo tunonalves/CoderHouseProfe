45aada7b2f1fb2150a4287a92321ea104b2ffb19
Federico Tuñon Alves <tunonalves@gmail.com>
2022-05-03 00:37:38 -0300

    url

--- django27620/ProyectoCoder/urls.py
+++ django27620/ProyectoCoder/urls.py
@@ -16,11 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
     path('AppCoder/', include('AppCoder.urls')),
     path('admin/', admin.site.urls),
-    
-    
 ]
-
